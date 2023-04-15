f95670667d429171828e8b9813b334626eaddfe4
sumansauravmay <sumansauravmay12345@gmail.com>
2023-04-16 02:16:03 +0530

    get player list backend

--- Backend/routes/event.route.js
+++ Backend/routes/event.route.js
@@ -69,7 +69,7 @@ eventRouter.patch("/addplayers/:id", async (req, res) => {
 eventRouter.get("/playerDel/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    let data = await EventModel.find({ _id: id }).populate("players");
+    let data = await EventModel.findOne({ _id: id }).populate("players");
     res.send(data);
   } catch (err) {
     console.log(err);
